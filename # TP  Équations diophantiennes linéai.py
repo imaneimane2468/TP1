30c87f1cfdc6c1e0f6930ac3b705846b860ef77b
imaneimane2468 <imane.labzioui@uit.ac.ma>
2025-11-09 21:57:18 +0100

    Add files via upload

---
+++ # TP  Équations diophantiennes linéai.py
@@ -0,0 +1,50 @@
+def pgcd(a, b):
+    """Calcule le PGCD de deux entiers a et b selon l'algorithme d'Euclide."""
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def euclide_etendu(a, b):
+    """
+    Retourne (d, x0, y0) tels que :
+    a·x0 + b·y0 = d = pgcd(a, b)
+    → Théorème de Bézout
+    """
+    if b == 0:
+        return (a, 1, 0)
+    else:
+        d, x1, y1 = euclide_etendu(b, a % b)
+        return (d, y1, x1 - (a // b) * y1)
+
+
+a = int(input("Entrer a : "))
+b = int(input("Entrer b : "))
+c = int(input("Entrer c : "))
+
+
+d, x0, y0 = euclide_etendu(a, b)
+print(f"\nPGCD({a},{b}) = {d}")
+
+
+print("\n--- Théorème de Bézout ---")
+print(f"Il existe des entiers x₀ et y₀ tels que :")
+print(f"{a}·({x0}) + {b}·({y0}) = {d}")
+
+
+if c % d != 0:
+    print("\n→ Pas de solution entière (d ne divise pas c).")
+else:
+    print("\n→ Il existe des solutions entières à l’équation ax + by = c.")
+
+   
+    x_p = x0 * (c // d)
+    y_p = y0 * (c // d)
+    print(f"\nSolution particulière : x = {x_p}, y = {y_p}")
+
+    
+    print("\nSolution générale :")
+    print(f"(x, y) = ({x_p} + {b//d}·k, {y_p} - {a//d}·k),   k ∈ ℤ")
+
+print("\n--------------------------------------------")
+print("Fin du programme – TP1 Équations Diophantiennes (EST Kénitra)")
+print("--------------------------------------------")
